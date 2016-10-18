669529d86b911c1eaba4f5d95278f864c22279de
Ray <marxspawn@gmail.com>
2016-10-17 23:56:59 -0400

    FinalBackup
    Tests completed in 54 milliseconds.
    71 assertions of 71 passed, 0 failed.

--- topics/about_this.js
+++ topics/about_this.js
@@ -4,19 +4,8 @@ test("'this' inside a method", function () {
 	var person = {
 		name: 'bob',
 		intro: function () {
-			return "Hello, my name is " + this.function;
+			return "Hello, my name is " + this.__;
 		}
 	}
 	equal(person.intro(), "Hello, my name is undefined", "If an object has a method can you access properties inside it?");
-});
-
-test("'this' on unattached function", function () {
-	var person = {
-		globalName: 'bob',
-		intro: function () {
-			return "Hello, my name is " + this.globalName;
-		}
-	}
-
-	var alias = person.intro;
 });
