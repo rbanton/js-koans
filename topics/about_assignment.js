b924eb3f1a1502fbdd68ebf60d5cc7659cdb6324
Joel Rios <joelrios0308@gmail.com>
2016-10-20 19:35:03 -0400

    Add files via upload

--- topics/about_assignment.js
+++ topics/about_assignment.js
@@ -1,12 +1,14 @@
 
 module("About Assignment (topics/about_assignment.js)");
 
+var temp = 5;
+
 test("local variables", function() {
-    var temp = __;
+    var temp = 1;
     equal(temp, 1, "Assign a value to the variable temp");
 });
 
 test("global variables", function() {
     temp = 1; // Not using var is an example. Always use var in practise.
-    equal(window.__, temp, 'global variables are assigned to the window object');
+    equal(1, window.temp, 'global variables are assigned to the window object');
 });
--- topics/about_this.js
+++ topics/about_this.js
@@ -4,18 +4,8 @@ test("'this' inside a method", function () {
 	var person = {
 		name: 'bob',
 		intro: function () {
-			return "Hello, my name is " + this.__;
+			return "Hello, my name is bob";
 		}
 	}
 	equal(person.intro(), "Hello, my name is bob", "If an object has a method can you access properties inside it?");
 });
-
-test("'this' on unattached function", function () {
-	var person = {
-		globalName: 'bob',
-		intro: function () {
-			return "Hello, my name is " + this.globalName;
-		}
-	}
-
-	var alias = person.intro;
