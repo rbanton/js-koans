c2f01815d51ee3f2e5db30813ac9cad3055878f4
Antoine Stimphil <antoinedstimphil@gmail.com>
2016-10-20 16:41:49 -0400

    Completed Javascript Koans Project

--- topics/about_operators.js
+++ topics/about_operators.js
@@ -5,9 +5,9 @@ test("addition", function() {
   var result = 0;
   //starting i at 0, add i to result and increment i by 1 until i is equal to 5
   for (var i = 0; i <= 5; i++) {
-    result = result + i;
+    result += i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(15, result, "What is the value of result?");
 });
 
 test("assignment addition", function() {
@@ -16,15 +16,15 @@ test("assignment addition", function() {
     //the code below is just like saying result = result + i; but is more concise
     result += i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(15, result, "What is the value of result?");
 });
 
 test("subtraction", function() {
   var result = 5;
   for (var i = 0; i <= 2; i++) {
-    result = result - i;
+    result -= i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(2, result, "What is the value of result?");
 });
 
 test("assignment subtraction", function() {
@@ -32,7 +32,7 @@ test("assignment subtraction", function() {
   for (var i = 0; i <= 2; i++) {
     result -= i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(2, result, "What is the value of result?");
 });
 
 //Assignment operators are available for multiplication and division as well
@@ -43,5 +43,5 @@ test("modulus", function() {
   var x = 5;
   //again this is exactly the same as result = result % x
   result %= x;
-  equal(__, result, "What is the value of result?");
+  equal(0, result, "What is the value of result?");
 });
