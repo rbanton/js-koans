b5e0b59b51a1e003218559b2ee4b29297624d3a9
Kaidy-Ann Ellis <cammybronz1982@gmail.com>
2016-10-20 22:03:39 -0400

    Submit uncommitted tests
    Test 1

--- topics/about_operators.js
+++ topics/about_operators.js
@@ -1,4 +1,6 @@
 
+
+
 module("About Operators (topics/about_operators.js)");
 
 test("addition", function() {
@@ -7,7 +9,7 @@ test("addition", function() {
   for (var i = 0; i <= 5; i++) {
     result = result + i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(15, result, "What is the value of result?");
 });
 
 test("assignment addition", function() {
@@ -16,7 +18,7 @@ test("assignment addition", function() {
     //the code below is just like saying result = result + i; but is more concise
     result += i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(15, result, "What is the value of result?");
 });
 
 test("subtraction", function() {
@@ -24,7 +26,7 @@ test("subtraction", function() {
   for (var i = 0; i <= 2; i++) {
     result = result - i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(2, result, "What is the value of result?");
 });
 
 test("assignment subtraction", function() {
@@ -32,7 +34,7 @@ test("assignment subtraction", function() {
   for (var i = 0; i <= 2; i++) {
     result -= i;
   }
-  equal(__, result, "What is the value of result?");
+  equal(2, result, "What is the value of result?");
 });
 
 //Assignment operators are available for multiplication and division as well
@@ -43,5 +45,5 @@ test("modulus", function() {
   var x = 5;
   //again this is exactly the same as result = result % x
   result %= x;
-  equal(__, result, "What is the value of result?");
+  equal(0, result, "What is the value of result?");
 });
