abcdfe1ce3b1a6e3475b783454cc8b40055bb29c
Cjwilliams36 <cwilliams280@mail.valenciacollege.edu>
2016-10-23 09:40:06 -0400

    update

--- topics/about_control_structures.js
+++ topics/about_control_structures.js
@@ -32,10 +32,10 @@ test("for in", function() {
 
 test("ternary operator", function() {
 	var fruit = true ? "apple" : "orange";
-	equal(__, fruit, 'what is the value of fruit?');
+	equal("apple", fruit, 'what is the value of fruit?');
 
 	fruit = false ? "apple" : "orange";
-	equal(__, fruit, 'now what is the value of fruit?');
+	equal("orange", fruit, 'now what is the value of fruit?');
 });
 
 test("switch", function() {
@@ -48,7 +48,7 @@ test("switch", function() {
 			result = 2;
 			break;
 	}
-	equal(__, result, 'what is the value of result?');
+	equal(3, result, 'what is the value of result?');
 });
 
 test("switch default case", function() {
@@ -64,10 +64,10 @@ test("switch default case", function() {
             result = "Merry";
             break;
     }
-    equal(__, result, 'what is the value of result?');
+    equal("Merry" result, 'what is the value of result?');
 });
 
 test("null coalescing", function() {
     var result = null || "a value";
-    equal(__, result, 'what is the value of result?');
+    equal(null, result, 'what is the value of result?');
 });
